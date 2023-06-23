c1a39cc2ba68cf66eea779d6fb3ead2169b4b336
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-22 19:46:26 -0500

    taller 21

--- src/components/CaracolaMagica.vue
+++ src/components/CaracolaMagica.vue
@@ -1,14 +1,14 @@
 <template>
   <h1>Caracola Magica</h1>
-  <img
-    src="https://ichef.bbci.co.uk/news/800/cpsprodpb/8B70/production/_102469653_gettyimages-962792890.jpg"
-    alt="No se puede cargara la imagen"
-  />
-  <input v-model="pregunta" type="text" placeholder="Hazme una pregunta" />
-  <p>Recuerda terminar con un enter la pregunta</p>
-  <div>
-    <h2>Voy a pasar de año?</h2>
-    <h1>Si</h1>
+  <img v-if="rutaImagen"   v-bind:src="rutaImagen" alt="No se puede cargar la imagen" />
+  <div class="bg-dark"></div>
+  <div class="contenedor">
+    <input v-model="pregunta" type="text" placeholder="Hazme una pregunta" />
+    <p>Recuerda terminar con un enter la pregunta</p>
+    <div>
+      <h2>{{ pregunta }}</h2>
+      <h1>{{ respuesta }}</h1>
+    </div>
   </div>
 </template>
 
@@ -16,15 +16,74 @@
 
 <script>
 export default {
-data(){
+  data() {
     return {
-        pregunta: 'Hola Mundo'
-    }
-}
+      pregunta: "Hola Mundo",
+      respuesta: "",
+      rutaImagen: null,
+    };
+  },
+  watch: {
+    pregunta(value, oldValue) {
+      console.log(value);
+      console.log(oldValue);
 
+      if (value.includes("?")) {
+        console.log("Consumir el API");
+        this.consumirAPI();
+      }
+    },
+  },
+  methods: {
+    async consumirAPI() {
+      const respuesta = await fetch("https://yesno.wtf/api").then((r) =>
+        r.json()
+      );
 
+      const { answer, image } = respuesta;
+      console.log(respuesta);
+      console.log(answer);
+      console.log(image);
+
+      this.respuesta = answer;
+      this.rutaImagen = image;
+    },
+  },
 };
 </script>
 
 <style>
+img,.bg-dark {
+  height: 100vh;
+  width: 100wh;
+  left: 0px;
+  max-height: 100%;
+  max-width: 100%;
+  position: fixed;
+  top: 0px;
+}
+.bg-dark{
+  background-color: rgba(0, 0, 0, 0.8);
+}
+
+
+.contenedor {
+  position: relative;
+}
+
+input {
+  width: 250px;
+  padding: 10px 15px;
+  border-radius: 5px;
+  border:none;
+}
+
+p,h1,h2 {
+  color: white;
+}
+
+p{
+  font-size:20px ;
+  margin-top: 0px;
+}
 </style>
